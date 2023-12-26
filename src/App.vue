6a067435b23eee36a8526a46fbeef548de4d5bb5
Matthew Player <mattplayer@mac.com>
2023-12-26 10:21:58 +0000

    add welcome and time display

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import Timer from './components/Timer.vue'
+  const currentYear = new Date().getFullYear()
+  const startOfYear = Number(new Date(`01/01/${currentYear}`))
+  const now = Number(new Date())
+  const elapsed = now - startOfYear
+  const elapsedDays = Math.floor(elapsed / 1000 / 60 / 60 / 24)
+  
+  const secondsToMinutes = (seconds: number): string => {
+    return `${Math.floor(seconds / 60)}:${seconds % 60}`
+  }
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1>Welcome to the Plank Challenge {{ currentYear }}</h1>
   </header>
 
   <main>
-    <TheWelcome />
+    <div>
+      <h3>
+        Today's challenge is
+      </h3>
+    </div>
+    <div>
+      <h1>
+        {{ secondsToMinutes(elapsedDays) }}
+      </h1>
+    </div>
+    <div>
+      <h3>
+        seconds
+      </h3>
+    </div>
   </main>
 </template>
 
